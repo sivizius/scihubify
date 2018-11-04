63d6ce45971b4681beea2e29348761d814ceb436
sivizius <sivizius@ohai.su>
2018-11-04 14:05:34 +0100

    test

---
+++ make.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm $(find | grep -P "(\.xpi|\.crx|~)$" )
+zip -r -FS ../scihubify.zip *
+echo "do stuff, than type any character…"
+read -n1 ans
+git add .
+git commit -m "$1"
+git push
